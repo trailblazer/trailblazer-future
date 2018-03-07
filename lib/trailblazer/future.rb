a3d86ca0c787f3a018899b8065d43d951ac747da
Abdelkader Boudih <terminale@gmail.com>
2018-03-07 23:02:20 +0000

    nothing to see here

---
+++ lib/trailblazer/future.rb
@@ -0,0 +1,7 @@
+require "trailblazer/future/version"
+
+module Trailblazer
+  module Future
+    # Your code goes here...
+  end
+end
---
+++ lib/trailblazer/future/version.rb
@@ -0,0 +1,5 @@
+module Trailblazer
+  module Future
+    VERSION = "0.0.0"
+  end
+end
